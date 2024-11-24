b74033416e20f6113b321b07bfcbe707435f3ed0
CheChe903 <cheche903@naver.com>
2024-11-24 16:29:47 +0900

    feat(Badge): 배지 객체 단위 테스트 생성

---
+++ src/test/java/christmas/BadgeTest.java
@@ -0,0 +1,20 @@
+package christmas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BadgeTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "6000, 별",
+            "12000, 트리",
+            "25000, 산타",
+            "3000, 없음"
+    })
+    void 뱃지_테스트(int price, Badge expectedBadge) {
+        assertEquals(expectedBadge, Badge.from(price));
+    }
+}
