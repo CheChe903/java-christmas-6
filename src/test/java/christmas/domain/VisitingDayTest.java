a075062b99c270b0013879cc2ecea74a60f601cb
CheChe903 <cheche903@naver.com>
2024-11-24 15:32:17 +0900

    feat(VisitingDayTest): 해당 객체 단위 테스트 진행
    1. 입력 날짜에 따라 할인 금액이 결정됩니다.

--- src/test/java/christmas/domain/VisitingDayTest.java
+++ src/test/java/christmas/domain/VisitingDayTest.java
@@ -1,5 +1,6 @@
 package christmas.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -11,5 +12,17 @@ class VisitingDayTest {
         //when & then
         assertThatThrownBy(() -> new VisitingDay(0))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력_날짜에_따라_할인_금액이_결정된다() {
+        //given
+        VisitingDay visitingDay = new VisitingDay(5);
+
+        //when
+        int discountFee = visitingDay.calculateFee();
+
+        //then
+        assertThat(discountFee).isEqualTo(1500);
     }
 }
